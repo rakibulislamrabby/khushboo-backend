f0ed60b9cb16edf0989af38d06a12e81bd3ff213
Rakibulislamrabby <rakibulislamrabby64@gmai.com>
2022-05-08 02:16:20 +0600

    create delete api

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ async function run() {
             const result = await itemsCollection.insertOne(newItem);
             res.send(result);
         })
+
+        app.delete("/inventory/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemsCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
